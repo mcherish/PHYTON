4a00e688dc92627abaa827be901ee979729e62a2
mcherish <33217345+mcherish@users.noreply.github.com>
2020-02-21 00:10:46 +0800

    python study

---
+++ class21.py
@@ -0,0 +1,25 @@
+def add(a,b):
+    print(f"Adding {a} + {b}")
+    return a + b
+
+def subtract(a,b):
+    print(f"Subtract {a} - {b}")
+    return a - b
+
+def multiply(a,b):
+    print(f"Multiply {a} * {b}")
+    return a * b
+
+def divice(a,b):
+    print(f"Dividing {a} / {b}")
+    return a / b
+
+a = add(1,2)
+
+b = subtract(3,4)
+
+c = multiply(5,6)
+
+d = float(divice(7,8))
+
+print(f"{a},{b},{c},{d}")
