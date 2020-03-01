dc1ab84119be3e922fe7473af3f56244e8163a57
mcherish <33217345+mcherish@users.noreply.github.com>
2020-03-01 22:24:31 +0800

    python study

---
+++ class28.py
@@ -0,0 +1,2 @@
+result = ("test" == 1)
+print(result)
---
+++ class29.py
@@ -0,0 +1,28 @@
+people = 20
+cats = 30
+dogs = 15
+
+if(people<dogs):
+    print("Too many cats!")
+
+if people > cats:
+    print("Not many people!")
+
+if people < dogs:
+    print("The world is drooled on!")
+
+if people > dogs:
+    print("The world is dry!")
+
+dogs += 5 
+
+if people >= dogs:
+    print("people are great than or equal to dogs.")
+
+if people <= dogs:
+    print("People are less than or equal to dogs.")
+
+if people == dogs:
+
+    print("People are dogs.")
+
